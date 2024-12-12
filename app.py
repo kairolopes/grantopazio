c997a0204e0b4c25c78e73cf7f55fada9a10b03b
kairolopes <151247602+kairolopes@users.noreply.github.com>
2024-12-12 19:12:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,17 @@
-
 import json
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 import faiss
 import os
 
-# Carregar o JSON do arquivo 'respostas_drigor.json'
-json_path = "respostas_drigor.json"
+# Carregar o JSON do arquivo no diretório atual
+json_path = "Perguntas_e_respostas.json"
 
 with open(json_path, "r") as file:
     data = json.load(file)
 
-# Configurar modelo e índice FAISS com as perguntas do JSON
-questions = [
-    "O que é o Chip da Beleza?",
-    "Qual é o endereço da clínica?",
-    "Quais são os horários disponíveis para consultas?",
-    "Como são realizadas as consultas online?",
-    "Qual é o valor da consulta?",
-    "Quais são as formas de pagamento aceitas?",
-    "Quem é o Dr. Igor Barcelos?"
-]
-
-# Mapear perguntas para suas respectivas chaves no JSON
-question_to_key = {
-    "O que é o Chip da Beleza?": "chip_da_beleza",
-    "Qual é o endereço da clínica?": "endereco_clinica",
-    "Quais são os horários disponíveis para consultas?": "horarios_consulta",
-    "Como são realizadas as consultas online?": "consulta_online",
-    "Qual é o valor da consulta?": "valores_consulta",
-    "Quais são as formas de pagamento aceitas?": "formas_pagamento",
-    "Quem é o Dr. Igor Barcelos?": "quem_e_dr_igor"
-}
-
+# Configurar modelo e índice FAISS
+questions = [item["question"] for item in data["perguntas_respostas"]]
 model = SentenceTransformer('all-MiniLM-L6-v2')
 embeddings = model.encode(questions)
 dimension = embeddings.shape[1]
@@ -45,7 +24,7 @@ app = Flask(__name__)
 # Adicionando o endpoint para a raiz
 @app.route('/')
 def home():
-    return jsonify({"message": "Bem-vindo à API do Dr. Igor Barcelos!"}), 200
+    return jsonify({"message": "Bem-vindo à API do Petshop!"}), 200
 
 @app.route('/get_answer', methods=['POST'])
 def get_answer():
@@ -53,18 +32,12 @@ def get_answer():
     if not user_question:
         return jsonify({"error": "A pergunta está vazia ou não foi enviada"}), 400
 
-    # Codificar a pergunta do usuário e procurar a correspondência mais próxima
     user_embedding = model.encode([user_question])
     _, indices = index.search(user_embedding, k=1)
     matched_question = questions[indices[0][0]]
+    response = next(item["resposta"] for item in data["perguntas_respostas"] if item["question"] == matched_question)
 
-    # Obter a chave correspondente no JSON e retornar a resposta
-    key = question_to_key.get(matched_question)
-    if key and key in data:
-        response = data[key]["resposta"]
-        return jsonify({"response": response})
-
-    return jsonify({"response": "Desculpe, não encontrei a informação solicitada."})
+    return jsonify({"response": response})
 
 # Adicionar endpoint de verificação de integridade
 @app.route('/health', methods=['GET'])
